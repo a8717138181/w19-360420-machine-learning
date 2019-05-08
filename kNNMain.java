fd5f390135bf6adb09ec3bb6758bfe978157e029
Zi An Gao <zi.gao@dawsoncollege.qc.ca>
2019-05-08 17:43:28 -0400

    features is done

--- kNNMain.java
+++ kNNMain.java
@@ -1,7 +1,7 @@
 import java.util.List;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
-
+import java.util.Scanner;
 
 
 public class kNNMain{
@@ -10,10 +10,47 @@ public class kNNMain{
 
     // TASK 1: Use command line arguments to point DataSet.readDataSet method to
     // the desired file. Choose a given DataPoint, and print its features and label
+	
+	 System.out.println("Please enter a file name that you want analyse");
+	System.out.println("breastCancer.csv           ");
+	System.out.println("data2DBinary.csv           ");
+	System.out.println("data2DOneVsAll.csv         ");
+	System.out.println("dataML.csv                 ");
+	System.out.println("handwrittenDigits.csv      ");
+	System.out.println("iris.csv                   ");
+	System.out.println("microchip.csv              ");
+
+	String input= null;
+	Scanner userinput = new Scanner(System.in); 
+	input = userinput.nextLine();	
+	DataSet.readDataSet("H:\\w19-360420-machine-learning\\data\\" + input);
+
+	
+	System.out.println("Please enter degree for list of dataPoints with higher order polynomial X");
+	Scanner in = new Scanner(System.in);
+	int degree = in.nextInt();
+	
+	DataSet.readDataSetHigherOrderFeatures("H:\\w19-360420-machine-learning\\data\\"+ input, degree);
+	
+	
+	
+	System.out.println("");
+	System.out.println("is Numeric ? " +  DataSet.isNumeric("H:\\w19-360420-machine-learning\\data\\"+ input));
+
+	
+	
+	
+	
+
+	//DataSet.getLabels("H:\\w19-360420-machine-learning\\data\\"+ input );
+
+		
+		
 
 
 
     //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
+	
 
 
     // TASK 3: Use the DataSet class methods to plot the 2D data (binary and multi-class)
